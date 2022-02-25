a1585e1ce2eec32a219554711c90dea80b1a9e2d
diegomarrufo <dmarrufo@ucm.es>
2022-02-25 16:13:28 +0100

    firebase api key clarification

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,3 +7,8 @@ export const firebaseConfig = {
 	appId: "1:198086792162:web:ac0e85494eb2393def5fbb",
 	measurementId: "G-Z3GYPMXGHM",
 };
+
+// For anyone wondering, according to these sites, it's safe to expose your Firebase Api Key as it's only an identifier for your project.
+// https://medium.com/@paulbreslin/is-it-safe-to-expose-your-firebase-api-key-to-the-public-7e5bd01e637b
+// https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public
+// https://stackoverflow.com/questions/52100690/should-i-hide-firebase-api-keys-into-backend-not-due-to-data-security-but-proje
